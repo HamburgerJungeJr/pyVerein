b8b5798996d1c98e895dc95c81cd746b3b718fc9
Jannik Heuer <Jannik@Heuer.Me>
2017-08-06 16:03:12 +0200

    Added member detail view.

--- pyVerein/members/urls.py
+++ pyVerein/members/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 # Set url-patterns
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<member_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- pyVerein/members/views.py
+++ pyVerein/members/views.py
@@ -1,9 +1,20 @@
-# Import django render shortcut
-from django.shortcuts import render
+# Import django render shortcut.
+from django.shortcuts import render, get_object_or_404 as get
+# Import members.
+from .models import Member
 
-# Index-View
+# Index-View.
 def index(request):
-    #Set Context
-    context = {}
+    # Get list of all members.
+    members = Member.objects.order_by('last_name').order_by('first_name')
+    # Set Context.
+    context = {'members': members}
     # Return rendered template.
     return render(request, 'members/index.html', context)
+
+# Detail-View.
+def detail(request, member_id):
+    # Get member.
+    member = get(Member, pk=member_id)
+    # Return rendered template.
+    return render(request, 'members/detail.html', {'member': member})
