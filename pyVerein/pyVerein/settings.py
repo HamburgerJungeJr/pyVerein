f6a185b50390e663a0b66256526e0e30eb36c19a
Jannik Heuer <Jannik@Heuer.Me>
2017-08-06 14:41:17 +0200

    Added mitglieder-app to django apps.

--- pyVerein/pyVerein/settings.py
+++ pyVerein/pyVerein/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mitglieder.apps.MitgliederConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
