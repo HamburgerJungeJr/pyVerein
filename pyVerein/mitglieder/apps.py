3b9b4ca90b984140f59ebf67e23ed57dc1210169
Jannik Heuer <Jannik@Heuer.Me>
2017-08-06 14:53:33 +0200

    Removed mitglieder

--- pyVerein/mitglieder/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MitgliederConfig(AppConfig):
-    name = 'mitglieder'
--- pyVerein/mitglieder/urls.py
+++
@@ -1,9 +0,0 @@
-# Import django-urls.
-from django.conf.urls import url
-# Import views.
-from . import views
-
-# Set url-patterns
-urlpatterns = [
-    url(r'^$', views.index, name='index')
-]
