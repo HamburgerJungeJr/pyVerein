731e2d672acecdf1f6278cf641eda6283bd65806
Jannik Heuer <Jannik@Heuer.Me>
2017-08-07 16:57:43 +0200

    Added Datatable view to members list.

--- pyVerein/members/urls.py
+++ pyVerein/members/urls.py
@@ -8,5 +8,6 @@ app_name = 'members'
 # Set url-patterns
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^api/list$', views.DatatableAPI.as_view(), name='apiList'),
     url(r'^(?P<member_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- pyVerein/members/views.py
+++ pyVerein/members/views.py
@@ -2,13 +2,16 @@
 from django.shortcuts import render, get_object_or_404 as get
 # Import members.
 from .models import Member
+# Import datatablesview.
+from django_datatables_view.base_datatable_view import BaseDatatableView
+# Import Q for extended filtering.
+from django.db.models import Q
 
 # Index-View.
 def index(request):
-    # Get list of all members.
-    members = Member.objects.order_by('last_name').order_by('first_name')
     # Set Context.
-    context = {'members': members}
+    context = {}
+
     # Return rendered template.
     return render(request, 'members/index.html', context)
 
@@ -16,5 +19,30 @@ def index(request):
 def detail(request, member_id):
     # Get member.
     member = get(Member, pk=member_id)
+
     # Return rendered template.
     return render(request, 'members/detail.html', {'member': member})
+
+# Datatable api view.
+class DatatableAPI(BaseDatatableView):
+    # Use Membermodel
+    model = Member
+
+    # Define displayed columns.
+    columns = ['last_name', 'first_name', 'street', 'zipcode', 'city']
+
+    # Define columns used for ordering.
+    order_columns = ['last_name', 'first_name', 'street', 'zipcode', 'city']
+
+    # Set maximum returned rows to prevent attacks.
+    max_rows = 500
+
+    # Filter rows.
+    def filter_request(self, qs):
+        # Read GET parameters.
+        GET = self.request.GET
+        if(search):
+            qs = qs.filter(Q(last_name__icontains=search) | Q(first_name__icontains=search) | Q(street__icontains=search) | Q(zipcode__icontains=search) | Q(city__icontains=search))
+
+        # Return filtered data.
+        return qs
