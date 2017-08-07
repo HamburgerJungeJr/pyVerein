03260fac7b093aee251994a08b052c0ecf46ac07
Jannik Heuer <Jannik@Heuer.Me>
2017-08-07 17:38:22 +0200

    Removed null from charFields.

--- pyVerein/members/models.py
+++ pyVerein/members/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-#Member model.
+# Member model.
 class Member(models.Model):
     # Lastname
     last_name = models.CharField(max_length=50)
     # Firstname
     first_name = models.CharField(max_length=50)
     # Street
-    street = models.CharField(blank=True, null=True, max_length=200)
+    street = models.CharField(blank=True, max_length=200)
     # Zipcode
-    zipcode = models.CharField(blank=True, null=True, max_length=10)
+    zipcode = models.CharField(blank=True, max_length=10)
     # City
-    city = models.CharField(blank=True, null=True, max_length=100)
+    city = models.CharField(blank=True, max_length=100)
     # Birthday
     birthday = models.DateField(blank=True, null=True)
 
