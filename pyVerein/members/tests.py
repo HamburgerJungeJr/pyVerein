8621ac2b9b9b69eaff73abef3016a0b460bb123d
Jannik Heuer <Jannik@Heuer.Me>
2017-08-07 17:39:04 +0200

    Added Member tests.

--- pyVerein/members/tests.py
+++ pyVerein/members/tests.py
@@ -1,3 +1,21 @@
+# Import TestCase.
 from django.test import TestCase
+# Import Membermodel.
+from .models import Member
 
-# Create your tests here.
+class MemberTestMethods(TestCase):
+    # Test for get_full_name method.
+    def test_get_full_name(self):
+        # Create Member.
+        member = Member.objects.create(first_name="first", last_name="last")
+
+        # Assert if full name is "first last".
+        self.assertEqual(member.get_full_name(), 'first last')
+
+    # Test for __str__ method.
+    def test__str__(self):
+        # Create Member.
+        member = Member.objects.create(first_name="first", last_name="last")
+
+        # Assert if __str__ ist full name.
+        self.assertEqual(str(member), "first last")
