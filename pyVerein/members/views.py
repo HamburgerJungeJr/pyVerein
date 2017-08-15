6aa49da6d5d9a77acc802f05599a7bd707211800
Jannik Heuer <Jannik@Heuer.Me>
2017-08-15 14:16:28 +0200

    Added members detail link to member list table

--- pyVerein/members/views.py
+++ pyVerein/members/views.py
@@ -1,11 +1,14 @@
 # Import django render shortcut.
 from django.shortcuts import render, get_object_or_404 as get
+# Import reverse.
+from django.urls import reverse
 # Import members.
 from .models import Member
 # Import datatablesview.
 from django_datatables_view.base_datatable_view import BaseDatatableView
 # Import Q for extended filtering.
 from django.db.models import Q
+
 
 # Index-View.
 def index(request):
@@ -15,6 +18,7 @@ def index(request):
     # Return rendered template.
     return render(request, 'members/index.html', context)
 
+
 # Detail-View.
 def detail(request, member_id):
     # Get member.
@@ -22,6 +26,7 @@ def detail(request, member_id):
 
     # Return rendered template.
     return render(request, 'members/detail.html', {'member': member})
+
 
 # Datatable api view.
 class DatatableAPI(BaseDatatableView):
@@ -38,11 +43,28 @@ class DatatableAPI(BaseDatatableView):
     max_rows = 500
 
     # Filter rows.
-    def filter_request(self, qs):
+    def filter_queryset(self, qs):
         # Read GET parameters.
-        GET = self.request.GET
-        if(search):
-            qs = qs.filter(Q(last_name__icontains=search) | Q(first_name__icontains=search) | Q(street__icontains=search) | Q(zipcode__icontains=search) | Q(city__icontains=search))
+        search = self.request.GET.get(u'search[value]', None)
+        if search:
+            qs = qs.filter(
+                Q(last_name__icontains=search) | Q(first_name__icontains=search) | Q(street__icontains=search) | Q(
+                    zipcode__icontains=search) | Q(city__icontains=search))
 
         # Return filtered data.
         return qs
+
+    # Prepare results to return as dict with urls
+    def prepare_results(self, qs):
+        # Initialize data array
+        json_data = []
+
+        # Loop through all items in queryset
+        for item in qs:
+            # Append dictionary with all columns and urls
+            json_data.append({'last_name': item.last_name, 'first_name': item.first_name, 'street': item.street,
+                              'zipcode': item.zipcode, 'city': item.city,
+                              'detail_url': reverse('members:detail', args=[item.id])})
+
+        # Return data
+        return json_data
