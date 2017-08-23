47762ed9c78f1e0ca35556065511d8381f71dd62
Jannik Heuer <Jannik@Heuer.Me>
2017-08-23 22:32:55 +0200

    Added render_ajax helper.

--- pyVerein/utils/views.py
+++ pyVerein/utils/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# Import HttpResponse
+from django.http import HttpResponse
+# Import template loader.
+from django.template import loader
+# Import JSON
+import json
+# Import Base64
+import base64
 
-# Create your views here.
+
+# Helper for rendering a template and return it as JSON with optional parameters.
+def render_ajax(request, template, context={}, additional_json={}):
+    # Load template.
+    template = loader.get_template(template)
+    # Render template.
+    renderedTemplate = template.render(context, request)
+
+    # Create JSON dict with additional json
+    json_data = additional_json
+    # Add Base64 encoded template to JSON
+    json_data['data'] = base64.b64encode(renderedTemplate.encode()).decode('utf-8')
+
+    # Return JSON.
+    return HttpResponse(json.dumps(json_data), 'application/json')
