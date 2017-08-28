705dc763ce968486840faaacd7a9d40abcd76565
Jannik Heuer <Jannik@Heuer.Me>
2017-08-28 23:44:48 +0200

    Added testcase for is_terminated method.

--- pyVerein/members/tests.py
+++ pyVerein/members/tests.py
@@ -2,6 +2,8 @@
 from django.test import TestCase
 # Import Membermodel.
 from .models import Member
+# Import datetime
+from datetime import datetime, timedelta
 
 class MemberTestMethods(TestCase):
     # Test for get_full_name method.
@@ -11,6 +13,32 @@ class MemberTestMethods(TestCase):
 
         # Assert if full name is "first last".
         self.assertEqual(member.get_full_name(), 'first last')
+
+    # Test for is_terminated method.
+    def test_is_terminated(self):
+        # Create Member.
+        member = Member.objects.create(first_name="first", last_name="last")
+
+        # New member should not be terminated
+        self.assertFalse(member.is_terminated())
+
+        # Set terminated date to future value
+        member.terminated_at = datetime.now().date() + timedelta(days=1)
+
+        # Member with terminated_at date in the future should not be terminated
+        self.assertFalse(member.is_terminated())
+
+        # Set terminated date to today value
+        member.terminated_at = datetime.now().date()
+
+        # Member with terminated_at date today should be terminated
+        self.assertTrue(member.is_terminated())
+
+        # Set terminated date to past value
+        member.terminated_at = datetime.now().date() - timedelta(days=1)
+
+        # Member with terminated_at date in the past should be terminated
+        self.assertTrue(member.is_terminated())
 
     # Test for __str__ method.
     def test__str__(self):
