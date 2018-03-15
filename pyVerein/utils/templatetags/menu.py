f43ff38db270a84891868a6cee445aad6e754ceb
Jannik <jannik@heuer.me>
2018-03-15 15:03:40 +0100

    Added base_url_class template tag

--- pyVerein/utils/templatetags/menu.py
+++ pyVerein/utils/templatetags/menu.py
@@ -9,6 +9,17 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def url_class(context, pattern_or_urlname, class_name):
     try:
+        pattern = '^' + reverse(pattern_or_urlname) + "$"
+    except NoReverseMatch:
+        pattern = pattern_or_urlname
+    path = context['request'].path
+    if re.search(pattern, path):
+        return class_name
+    return ''
+
+@register.simple_tag(takes_context=True)
+def base_url_class(context, pattern_or_urlname, class_name):
+    try:
         pattern = '^' + reverse(pattern_or_urlname)
     except NoReverseMatch:
         pattern = pattern_or_urlname
