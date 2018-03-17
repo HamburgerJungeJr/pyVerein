c8425090e4b037672e21fe381ab6d6d77b0330b9
Jannik <jannik@heuer.me>
2018-03-17 23:08:18 +0100

    Added Success alert

---
+++ pyVerein/app/static/app/js/keys.js
@@ -0,0 +1,23 @@
+(function($){
+    $.fn.nextOnEnter = function() {
+        this.keydown(function (event) {
+            if (event.which == 13) {
+                event.preventDefault();
+                var field = event.target;
+                if (field.getAttribute('data-next')) {
+                    var next = $('#' + field.getAttribute('data-next'));
+                    next.focus();
+                }
+            }
+        });
+    };  
+
+    $.fn.saveOnTimes = function() {
+        this.keydown(function (event) {
+            if (event.which == 106) {
+                event.preventDefault();
+                form.submit();
+            }
+        });
+    };  
+}(jQuery));
