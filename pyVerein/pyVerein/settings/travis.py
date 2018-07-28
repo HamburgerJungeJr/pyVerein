2cf98208b1905098b55cbbf2ebd369feb71b2685
Jannik <jannik@heuer.me>
2018-07-28 18:49:02 +0200

    dj-database-url

--- pyVerein/pyVerein/settings/travis.py
+++ pyVerein/pyVerein/settings/travis.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from .base import *
-import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
