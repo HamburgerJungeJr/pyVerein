9ccd46a3bb6bf155210824cd5c42fc520763b7bc
Jannik <jannik@heuer.me>
2018-07-28 18:56:28 +0200

    static files

--- pyVerein/pyVerein/settings/travis.py
+++ pyVerein/pyVerein/settings/travis.py
@@ -37,10 +37,10 @@ DATABASES = {
 # Static files
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
