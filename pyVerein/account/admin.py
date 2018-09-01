bca5f6f5e07b03d1d7e45730697fe44302f3fa8a
Jannik <jannik@heuer.me>
2018-09-01 14:09:14 +0200

    Added user password to admin

--- pyVerein/account/admin.py
+++ pyVerein/account/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+class AccountCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('avatar', )
+
+class AccountChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('avatar', )
+
+class AccountAdmin(UserAdmin):
+    add_form = AccountCreationForm
+    form = AccountChangeForm
+
+admin.site.register(User, AccountAdmin)
