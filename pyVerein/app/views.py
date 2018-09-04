22dae2f364975e7765516541c501bc0e767d2a67
Jannik <jannik@heuer.me>
2018-09-04 03:01:40 +0200

    Member chart

--- pyVerein/app/views.py
+++ pyVerein/app/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib import messages
+from django.db.models import Count
+from members.models import Division
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'app/dashboard.html', {})
+    divisions = Division.objects.annotate(members=Count('member'))
+    return render(request, 'app/dashboard.html', {'divisions': divisions})
