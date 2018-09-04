483f8d42e79f6b7290faa5aaecfb770b9207b0a5
Jannik <jannik@heuer.me>
2018-09-04 12:05:27 +0200

    Dashboard member fix

--- pyVerein/app/views.py
+++ pyVerein/app/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib import messages
 from django.db.models import Count
-from members.models import Division
+from members.models import Member
 
 # Create your views here.
 @login_required
 def index(request):
-    divisions = Division.objects.annotate(members=Count('member'))
+    divisions = Member.objects.values('division__name').annotate(members=Count('id'))
     return render(request, 'app/dashboard.html', {'divisions': divisions})
