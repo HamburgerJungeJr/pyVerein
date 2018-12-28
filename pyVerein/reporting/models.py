e26ec13b5c084173a7010ff51e736df66f95107a
Jannik Heuer <Jannik@Heuer.me>
2018-12-28 11:40:54 +0100

    Added Report/Resource models

--- pyVerein/reporting/models.py
+++ pyVerein/reporting/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from author.decorators import with_author
+from utils.models import ModelBase
+from django.utils.translation import ugettext_lazy as _
+from django.forms import ValidationError
 
-# Create your models here.
+class Report(ModelBase):
+    """
+    Model for storing reports
+    """
+
+    name = models.CharField(null=False, blank=False, max_length=255)
+    
+    description = models.TextField(null=True, blank=True)
+
+    report = models.FileField(null=True, blank=True, upload_to='reports/definition')
+        
+    def clean(self, *args, **kwargs):
+        super(Report, self).clean(*args, **kwargs)
+        if not self.report:
+            raise ValidationError(_('Report must not be empty'))
+
+def get_resource_path(instance, filename):
+    """
+    Return resourcepath with report id folder
+    """
+    return "reports/resource/{}/{}".format(instance.report.id, filename)
+
+class Resource(ModelBase):
+    """
+    Model for report-resource
+    """
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+
+    resource = models.FileField(null=False, blank=False, upload_to=get_resource_path)
+
