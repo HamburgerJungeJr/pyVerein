cde4822b17bf8b890e62922de3b4d0365dbad499
Jannik Heuer <Jannik@Heuer.me>
2018-12-29 17:01:20 +0100

    Added models to Report

--- pyVerein/reporting/forms.py
+++ pyVerein/reporting/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from .models import Report, Resource
-from django.forms.widgets import FileInput
-
-
+from django.forms.widgets import FileInput, CheckboxSelectMultiple
+from django.utils.translation import ugettext_lazy as _
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ('name', 'description', 'report',)
+        fields = ('name', 'description', 'report', 'model')
         widgets = {
             'report': FileInput(),
+            'model': CheckboxSelectMultiple(attrs={'class': 'mdc-checkbox__native-control'})
         }
 
 class ResourceForm(forms.ModelForm):
--- pyVerein/reporting/models.py
+++ pyVerein/reporting/models.py
@@ -4,23 +4,53 @@ from utils.models import ModelBase
 from django.utils.translation import ugettext_lazy as _
 from django.forms import ValidationError
 import uuid
+from separatedvaluesfield.models import SeparatedValuesField
 
 def get_report_path(instance, filename):
     """
     Return reportpath with report id folder
     """
-    return "reports/{}/definition/{}".format(instance.uuid, filename)
+    return "protected/reports/{}/definition/{}".format(instance.uuid, filename)
 
 class Report(ModelBase):
     """
     Model for storing reports
     """
 
+    class Meta:
+        permissions = (
+            ('run_report', 'Can run report'),
+        )
+
+    MEMBER = 'MEM'
+    DIVISION = 'DIV'
+    SUBSCRIPTION = 'SUB'
+    ACCOUNT = 'ACC'
+    COSTCENTER = 'COC'
+    COSTOBJECT = 'COO'
+    TRANSACTION = 'TRA'
+    CLOSURETRANSACTION = 'CTR'
+    CLOSUREBALANCE = 'CBA'
+
+    MODELS = (
+        (MEMBER, _('Member')),
+        (DIVISION, _('Division')),
+        (SUBSCRIPTION, _('Subscription')),
+        (ACCOUNT, _('Account')),
+        (COSTCENTER, _('Costcenter')),
+        (COSTOBJECT, _('Costobject')),
+        (TRANSACTION, _('Transaction')),
+        (CLOSURETRANSACTION, _('Closuretransaction')),
+        (CLOSUREBALANCE, _('Closurebalance'))
+    )
+
     name = models.CharField(null=False, blank=False, max_length=255)
     
     description = models.TextField(null=True, blank=True)
 
     report = models.FileField(null=True, blank=True, upload_to=get_report_path)
+
+    model = SeparatedValuesField(null=False, blank=False, max_length=255, choices=MODELS, default=[MEMBER])
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
         
@@ -33,7 +63,7 @@ def get_resource_path(instance, filename):
     """
     Return resourcepath with report id folder
     """
-    return "reports/{}/resource/{}".format(instance.report.uuid, filename)
+    return "protected/reports/{}/resource/{}".format(instance.report.uuid, filename)
 
 class Resource(ModelBase):
     """
