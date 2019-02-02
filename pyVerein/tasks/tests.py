163493a4b67ded35e9bc6f12d61e94d6ca3bd6cc
Jannik Heuer <Jannik@Heuer.me>
2019-02-02 19:40:17 +0100

    Added delete_report_data task

--- pyVerein/tasks/tests.py
+++ pyVerein/tasks/tests.py
@@ -126,7 +126,7 @@ class TasksTestMethods(TestCase):
         response = self.client.get(reverse('tasks:apply_annualclosure'))
         self.assertEqual(response.status_code, 400)
     
-    def test_run_subscription_task_permission(self):
+    def test_run_delete_terminated_members_task_permission(self):
         "User should only access delete terminated members task if view, run and delete_terminated_members_task permissions are set"
 
         user = User.objects.get(username='temp')
@@ -174,4 +174,54 @@ class TasksTestMethods(TestCase):
         user.user_permissions.add(Permission.objects.get(codename='run_tasks'))
         user.user_permissions.add(Permission.objects.get(codename='run_delete_terminated_members_task'))
         response = self.client.get(reverse('tasks:delete_terminated_members'))
+        self.assertEqual(response.status_code, 400)
+
+    def test_run_delete_report_data_task_permission(self):
+        "User should only access delete report data task if view, run and delete_report_data_task permissions are set"
+
+        user = User.objects.get(username='temp')
+        
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.add(Permission.objects.get(codename='view_tasks'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.remove(Permission.objects.get(codename='view_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_tasks'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.remove(Permission.objects.get(codename='run_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_delete_report_data_task'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.remove(Permission.objects.get(codename='run_delete_report_data_task'))
+        user.user_permissions.add(Permission.objects.get(codename='view_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_tasks'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.remove(Permission.objects.get(codename='view_tasks'))
+        user.user_permissions.remove(Permission.objects.get(codename='run_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_delete_report_data_task'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.remove(Permission.objects.get(codename='run_tasks'))
+        user.user_permissions.remove(Permission.objects.get(codename='run_delete_report_data_task'))
+        user.user_permissions.add(Permission.objects.get(codename='view_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_delete_report_data_task'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
+        self.assertEqual(response.status_code, 403)
+
+        user.user_permissions.remove(Permission.objects.get(codename='view_tasks'))
+        user.user_permissions.remove(Permission.objects.get(codename='run_delete_report_data_task'))
+        user.user_permissions.add(Permission.objects.get(codename='view_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_tasks'))
+        user.user_permissions.add(Permission.objects.get(codename='run_delete_report_data_task'))
+        response = self.client.get(reverse('tasks:delete_report_data'))
         self.assertEqual(response.status_code, 400)
