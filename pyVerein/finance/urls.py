13ecf8ac3ca48460f8fe43a7ec031cddeb285268
Jannik Heuer <Jannik@Heuer.me>
2019-04-15 16:53:07 +0200

    Reordered urls

--- pyVerein/finance/urls.py
+++ pyVerein/finance/urls.py
@@ -8,38 +8,38 @@ app_name = 'finance'
 # Set url-patterns
 urlpatterns = [
     path('creditor/', views.CreditorIndexView.as_view(), name='creditor_list'),
+    path('creditor/new/', views.CreditorCreateView.as_view(), name='creditor_create'),
     path('creditor/<str:pk>/', views.CreditorDetailView.as_view(), name='creditor_detail'),
     path('creditor/<str:pk>/edit/', views.CreditorEditView.as_view(), name='creditor_edit'),
-    path('creditor/new/', views.CreditorCreateView.as_view(), name='creditor_create'),
     path('creditor/<str:account>/clearing/', views.CreditorClearingView.as_view(), name='creditor_clear'),
 
     path('debitor/', views.DebitorIndexView.as_view(), name='debitor_list'),
+    path('debitor/new/', views.DebitorCreateView.as_view(), name='debitor_create'),
     path('debitor/<str:pk>/', views.DebitorDetailView.as_view(), name='debitor_detail'),
     path('debitor/<str:pk>/edit/', views.DebitorEditView.as_view(), name='debitor_edit'),
-    path('debitor/new/', views.DebitorCreateView.as_view(), name='debitor_create'),
     path('debitor/<str:account>/clearing/', views.DebitorClearingView.as_view(), name='debitor_clear'),
 
     path('impersonal/', views.ImpersonalIndexView.as_view(), name='impersonal_list'),
+    path('impersonal/new/', views.ImpersonalCreateView.as_view(), name='impersonal_create'),
     path('impersonal/<str:pk>/', views.ImpersonalDetailView.as_view(), name='impersonal_detail'),
     path('impersonal/<str:pk>/edit/', views.ImpersonalEditView.as_view(), name='impersonal_edit'),
-    path('impersonal/new/', views.ImpersonalCreateView.as_view(), name='impersonal_create'),
 
     path('costcenter/', views.CostCenterIndexView.as_view(), name='costcenter_list'),
+    path('costcenter/new/', views.CostCenterCreateView.as_view(), name='costcenter_create'),
     path('costcenter/<str:pk>/', views.CostCenterDetailView.as_view(), name='costcenter_detail'),
     path('costcenter/<str:pk>/edit/', views.CostCenterEditView.as_view(), name='costcenter_edit'),
-    path('costcenter/new/', views.CostCenterCreateView.as_view(), name='costcenter_create'),
 
     path('costobject/', views.CostObjectIndexView.as_view(), name='costobject_list'),
+    path('costobject/new/', views.CostObjectCreateView.as_view(), name='costobject_create'),
     path('costobject/<str:pk>/', views.CostObjectDetailView.as_view(), name='costobject_detail'),
     path('costobject/<str:pk>/edit/', views.CostObjectEditView.as_view(), name='costobject_edit'),
-    path('costobject/new/', views.CostObjectCreateView.as_view(), name='costobject_create'),
 
     path('transaction/', views.TransactionIndexView.as_view(), name='transaction_list'),
-    path('transaction/<int:internal_number>/', views.TransactionDetailView.as_view(), name='transaction_detail'),
-    path('transaction/<int:internal_number>/edit/<int:pk>/', views.TransactionEditView.as_view(), name='transaction_edit'),
     path('transaction/new/', views.TransactionCreateView.as_view(), name='transaction_create'),
     path('transaction/new/<str:session_id>/', views.TransactionCreateView.as_view(), name='transaction_create_session'),
     path('transaction/new/<str:session_id>/<int:step>/', views.TransactionCreateView.as_view(), name='transaction_create_step'),
+    path('transaction/<int:internal_number>/', views.TransactionDetailView.as_view(), name='transaction_detail'),
+    path('transaction/<int:internal_number>/edit/<int:pk>/', views.TransactionEditView.as_view(), name='transaction_edit'),
     path('transaction/<int:internal_number>/reset', views.reset_transaction, name='transaction_reset'),
     path('transaction/<int:internal_number>/reset_new', views.reset_new_transaction, name='transaction_reset_new'),
 
--- pyVerein/members/urls.py
+++ pyVerein/members/urls.py
@@ -8,20 +8,20 @@ app_name = 'members'
 # Set url-patterns
 urlpatterns = [
     path('member/', views.MemberIndexView.as_view(), name='member_list'),
+    path('member/new/', views.MemberCreateView.as_view(), name='member_create'),
     path('member/<int:pk>/', views.MemberDetailView.as_view(), name='member_detail'),
     path('member/edit/<int:pk>/', views.MemberEditView.as_view(), name='member_edit'),
-    path('member/new/', views.MemberCreateView.as_view(), name='member_create'),
     path('upload_file/<int:pk>/', views.upload_file, name='member_upload_file'),
     path('delete_file/<int:pk>/', views.delete_file, name='member_delete_file'),
     path('download_file/<int:pk>/', views.download_file, name='member_download_file'),
 
     path('division/', views.DivisionIndexView.as_view(), name='division_list'),
+    path('division/new/', views.DivisionCreateView.as_view(), name='division_create'),
     path('division/<int:pk>/', views.DivisionDetailView.as_view(), name='division_detail'),
     path('division/edit/<int:pk>/', views.DivisionEditView.as_view(), name='division_edit'),
-    path('division/new/', views.DivisionCreateView.as_view(), name='division_create'),
 
     path('subscription/', views.SubscriptionIndexView.as_view(), name='subscription_list'),
+    path('subscription/new/', views.SubscriptionCreateView.as_view(), name='subscription_create')
     path('subscription/<int:pk>/', views.SubscriptionDetailView.as_view(), name='subscription_detail'),
     path('subscription/edit/<int:pk>/', views.SubscriptionEditView.as_view(), name='subscription_edit'),
-    path('subscription/new/', views.SubscriptionCreateView.as_view(), name='subscription_create')
 ]
